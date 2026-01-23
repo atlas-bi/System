c49466746f7780c50f8ca272f9e4d05d2b29e524
Semahegn <semahegn.adugna1@gmail.com>
2026-01-23 16:13:33 +0300

    test: register cypress custom commands

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,6 +1,53 @@
 import '@testing-library/cypress/add-commands';
 
-import './commands.ts';
+import { faker } from '@faker-js/faker';
+
+function login({
+	email = faker.internet.email(undefined, undefined, 'example.com'),
+}: {
+	email?: string;
+} = {}) {
+	cy.then(() => ({ email })).as('user');
+	cy.exec(
+		`npx ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts "${email}"`,
+	).then(({ stdout }) => {
+		const cookieValue = stdout
+			.replace(/.*<cookie>(?<cookieValue>.*)<\/cookie>.*/s, '$<cookieValue>')
+			.trim();
+		cy.setCookie('__session', cookieValue);
+	});
+	return cy.get('@user');
+}
+
+function deleteUserByEmail(email: string) {
+	cy.exec(
+		`npx ts-node --require tsconfig-paths/register ./cypress/support/delete-user.ts "${email}"`,
+	);
+	cy.clearCookie('__session');
+}
+
+function cleanupUser({ email }: { email?: string } = {}) {
+	if (email) {
+		deleteUserByEmail(email);
+	} else {
+		cy.get('@user').then((user) => {
+			const email = (user as { email?: string }).email;
+			if (email) {
+				deleteUserByEmail(email);
+			}
+		});
+	}
+	cy.clearCookie('__session');
+}
+
+function visitAndCheck(url: string, waitTime: number = 1000) {
+	cy.visit(url);
+	cy.location('pathname').should('contain', url).wait(waitTime);
+}
+
+Cypress.Commands.add('login', login);
+Cypress.Commands.add('cleanupUser', cleanupUser);
+Cypress.Commands.add('visitAndCheck', visitAndCheck);
 
 Cypress.on('uncaught:exception', (err) => {
 	// Cypress and React Hydrating the document don't get along
