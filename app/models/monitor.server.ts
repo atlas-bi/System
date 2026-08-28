e43fe12fdf6929203ffac12897a566af251599f4
Christopher Pickering <christopher@landandsea.ch>
2026-08-28 15:33:10 +0200

    fixed cert notification

--- app/models/monitor.server.ts
+++ app/models/monitor.server.ts
@@ -1351,8 +1351,8 @@ export function updateMonitor({
 		cpuProcessors?: string;
 		lastBootTime?: string | null | Date;
 		cpuMaxSpeed?: string | null;
-		certDays?: string;
-		certValid?: boolean;
+		certDays?: string | null;
+		certValid?: boolean | null;
 	};
 	feed?: {
 		memoryFree?: string;
